15be8d3d4485b499a58c715040cfe89d2f4b3eb9
Josip <jomihx@gmail.com>
2020-04-03 16:31:29 +0200

    da vidimo radi li

--- funkcionalnosti.js
+++ funkcionalnosti.js
@@ -1,9 +1,10 @@
 
 
 //izmjene za rezolucije
+$(document).ready(function () {
 if ($(window).width() <= 1024) {
     alert("radi")
 }
  else if ($(window).width() <= 600){
     $("backLink").text("<")
- }
+ }})
