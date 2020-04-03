957141fdab610b35acdd09f3961557b508cbf783
Josip <jomihx@gmail.com>
2020-04-03 17:09:42 +0200

    užas

--- funkcionalnosti.js
+++ funkcionalnosti.js
@@ -5,6 +5,7 @@
 $(document).ready(function () {
     function checkWidth() {
         if ($(window).width() <= 1024) {
+            $(".LeftMenuHolder").hide()
         }
         else if ($(window).width() <= 600) {
             $("a.backLink").hide()
