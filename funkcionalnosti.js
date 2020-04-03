22330a7a099921c41985bf7b510a2eca979d6f77
Josip <jomihx@gmail.com>
2020-04-03 16:39:17 +0200

    funkcije 6

--- funkcionalnosti.js
+++ funkcionalnosti.js
@@ -11,9 +11,10 @@ var windowsize = $(window).width();
 $(window).resize(function() {
   windowsize = $(window).width();
   if ($(window).width() <= 1024) {
-    alert("radi")
 }
  else if ($(window).width() <= 600){
     $(".backLink").text("<")
+    alert("radi")
+
  }
 });
