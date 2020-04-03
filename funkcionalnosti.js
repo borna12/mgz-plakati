10eeb2c2a007354913e2b4bdb9c0bc6c957ae056
Josip <jomihx@gmail.com>
2020-04-03 16:22:50 +0200

    funkcije2

--- funkcionalnosti.js
+++ funkcionalnosti.js
@@ -1,8 +1,8 @@
 
 //izmjene za rezolucije
 if ($(window).width() <= 1024) {
-
+    alert("radi")
 }
- else if ($(window).width() < 600){
-    $(".header a.backLink").text("<")
+ else if ($(window).width() <= 600){
+    $("backLink").text("<")
  }
