9cc689d5934090df2adc9f912cf99c6c27ff2910
Josip <jomihx@gmail.com>
2020-04-03 16:33:21 +0200

    funckije 5

--- funkcionalnosti.js
+++ funkcionalnosti.js
@@ -1,10 +1,19 @@
 
 
 //izmjene za rezolucije
-$(document).ready(function () {
-if ($(window).width() <= 1024) {
+
+
+ 
+
+
+var windowsize = $(window).width();
+
+$(window).resize(function() {
+  windowsize = $(window).width();
+  if ($(window).width() <= 1024) {
     alert("radi")
 }
  else if ($(window).width() <= 600){
-    $("backLink").text("<")
- }})
+    $(".backLink").text("<")
+ }
+});
