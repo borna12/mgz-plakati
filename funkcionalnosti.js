5e147ce85c272990b0af474e4dc774aa90ceaddc
Josip <jomihx@gmail.com>
2020-04-04 14:22:57 +0200

    responzivnost

--- funkcionalnosti.js
+++ funkcionalnosti.js
@@ -1,5 +1,8 @@
 $('.LeftMenuHolder, .RightContentHolder, .ContentHolder').wrapAll('<div class="section group">');
-//izmjene za rezolucije
+$('.LeftMenuHolder').addClass("col span_1_of_3")
+$('.RightContentHolder').addClass("col span_1_of_3")
+$('.ContentHolder').addClass("col span_1_of_3")
+    //izmjene za rezolucije
 $(document).ready(function() {
     function checkWidth() {
         if ($(window).width() <= 600) {
