93af7f8e7def39b7e649f958e5728b5d1a9b778f
Josip <jomihx@gmail.com>
2020-04-04 14:11:29 +0200

    da vidimo 2

--- funkcionalnosti.js
+++ funkcionalnosti.js
@@ -1,6 +1,5 @@
+$('.LeftMenuHolder, .RightContentHolder, .ContentHolder').wrapAll('<div class="section group">');
 //izmjene za rezolucije
-$(".LeftMenuHolder").before("<div class='section group'>")
-$(".RightContentHolder").after("</div>")
 $(document).ready(function() {
     function checkWidth() {
         if ($(window).width() <= 600) {
