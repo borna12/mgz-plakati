ccb0af8086ff16595240da0bf2ea7566aebe84c3
Josip <jomihx@gmail.com>
2020-04-05 16:09:50 +0200

    da vidimo

--- funkcionalnosti.js
+++ funkcionalnosti.js
@@ -49,6 +49,20 @@ $(document).ready(function() {
         $('#mySidenav').show();
     })
 
+
+    window.onscroll = function() { myFunction() };
+
+    var header = document.getElementById("mySidenav");
+    var sticky = header.offsetTop;
+
+    function myFunction() {
+        if (window.pageYOffset > sticky) {
+            header.classList.add("sticky");
+        } else {
+            header.classList.remove("sticky");
+        }
+    }
+
 });
 
 
