88eadd023d9657cc245b24fcac9d265cc977152c
Josip <jomihx@gmail.com>
2020-04-06 13:55:07 +0200

    da vidimo

--- funkcionalnosti.js
+++ funkcionalnosti.js
@@ -1,3 +1,8 @@
+prestani = 0
+if ($(location).attr('href') == "http://mgz-plakati.web.link2.hr/" || $(location).attr('href') == "http://mgz-plakati.web.link2.hr/en" || $(location).attr('href') == "http://mgz-plakati.web.link2.hr/hr") {
+    localStorage.clear()
+}
+
 $.fn.extend({
     toggleText: function(a, b) {
         return this.text(this.text() == b ? a : b);
